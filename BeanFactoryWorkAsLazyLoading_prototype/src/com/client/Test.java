bab5422176d583f093c76211e4f68f460d4fb3f4
B153sharayu <sharayughodeswar123@gmail.com>
2023-03-21 12:51:41 +0530

    spring

---
+++ BeanFactoryWorkAsLazyLoading_prototype/src/com/client/Test.java
@@ -0,0 +1,16 @@
+package com.client;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		
+	Resource r = new ClassPathResource("bean.xml");
+	BeanFactory bf = new XmlBeanFactory(r);
+	            bf.getBean("s");
+	}
+}
