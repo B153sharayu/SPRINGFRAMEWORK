ab0291841e3e34661826e47f7d4331b812b4be86
B153sharayu <sharayughodeswar123@gmail.com>
2023-03-27 23:36:58 +0530

    spring loded

---
+++ AutowireWithAnnotationBased/src/com/client/Test.java
@@ -0,0 +1,22 @@
+package com.client;
+
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.model.Student;
+
+public class Test {
+
+public static void main(String[] args) {
+		
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		
+		Student s = ac.getBean("s",Student.class);
+		System.out.println(s.getRollno());
+		System.out.println(s.getName());
+		System.out.println(s.getAdr().getAreaname());
+		System.out.println(s.getAdr().getCityname());
+	
+	}
+}
